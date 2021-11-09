b2b1ced8165ca6181fc1c2cc768cf2b833d23cf6
julianaaquinoo <juteixeira2012@gmail.com>
2021-11-09 11:51:04 -0300

    1 file modified, 1 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,28 +1,36 @@
 export interface Product {
   id: number;
-  name: string;
-  price: number;
-  description: string;
+  cep: number;
+  logradouro: string;
+  cidade: string;
+  bairro: string;
+  uf: string;
 }
 
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    cep: 18105000,
+    logradouro: 'Parana',
+    cidade: 'sorocaba',
+    bairro: 'Cajuru do Sul',
+    uf: 'são paulo',
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    cep: 93212260,
+    logradouro: 'hugo gerdau',
+    cidade: 'sapucaia do sul',
+    bairro: 'são jorge',
+    uf: 'sapucaia do sul',
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    cep: 91360-390,
+    logradouro: 'PDoutor Dário de Bittencourt',
+    cidade: 'porto alegre',
+    bairro: 'Jardim Europa',
+    uf: 'porto alegre',
   }
 ];
 
